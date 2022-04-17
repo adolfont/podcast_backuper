1b53c226bcaf9e61de823acb8720772849216191
Adolfo Neto <adolfo.usp@gmail.com>
2022-04-17 18:18:59 -0300

    lists titles and links of episodes

--- lib/podcast_backuper.ex
+++ lib/podcast_backuper.ex
@@ -22,6 +22,39 @@ defmodule PodcastBackuper do
   end
 end
 
-result = PodcastBackuper.read_RSS("https://anchor.fm/s/248c0568/podcast/rss")
+import SweetXml
 
-IO.puts(result.body)
+url = "https://anchor.fm/s/248c0568/podcast/rss"
+doc = PodcastBackuper.read_RSS(url)
+title = doc.body |> xpath(~x"//channel/title/text()")
+description = doc.body |> xpath(~x"//channel/description/text()")
+
+IO.puts("RSS URL #{url}")
+IO.puts("The title of this podcast is \"#{title} \"")
+IO.puts("The description of this podcast is:")
+IO.puts(description)
+
+episode_titles =
+  doc.body
+  |> xpath(~x"//item/title/text()"sl)
+
+IO.inspect(episode_titles)
+
+links =
+  doc.body
+  |> xpath(~x"//item/link/text()"sl)
+
+IO.inspect(links)
+
+# audio_file_links =
+#   doc.body
+#   |> xpath(~x"//item/enclosure"el)
+#   # |> Enum.map(fn x -> to_string(x) end)
+
+# IO.inspect(audio_file_links)
+
+# episodes =
+#   doc.body
+#   |> xpath(~x"//channel/item")
+
+# IO.inspect(episodes)
